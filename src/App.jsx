0e5fc3b765dcb8b656b687c18a41f4e642845ea5
Atharva-Deshmukh1 <atharva25deshmukh@gmail.com>
2024-01-09 23:34:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import InputBox from './components/InputBox'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-500'>hi</h1>
+      <InputBox />
+      <InputBox />
+    </>
+  )
+}
+
+export default App
